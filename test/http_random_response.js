f6cf449731837932ac1e0922a121a98f1d398a85
chrulri <chrulri@gmail.com>
2019-01-23 00:29:41 +0100

    implement json schema validation

---
+++ test/http_random_response.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer();
+server.on('request', (req,res) => {
+	console.log(`${req.method} ${req.url}`);
+	res.setHeader('Content-Type','application/json');
+	res.end('{"hello":"world"}');
+});
+server.listen(8080);
+
